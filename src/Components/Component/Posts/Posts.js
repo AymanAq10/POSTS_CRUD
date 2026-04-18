1acb7037d4e4778a475f56fd0c353aea543c917c
AymanAq10 <121875996+AymanAq10@users.noreply.github.com>
2026-04-19 00:27:42 +0100

    Reduce loading delay from 3000ms to 1000ms

--- src/Components/Component/Posts/Posts.js
+++ src/Components/Component/Posts/Posts.js
@@ -25,7 +25,7 @@ const Posts = () => {
                         seterrMsg(err.message);
                     });
                 }
-                ,3000)
+                ,1000)
 
     }, []);
     const Delete = (id) => {
